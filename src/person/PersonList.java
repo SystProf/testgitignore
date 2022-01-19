83fd67413f92f962ca0d233d5b4e21fb5caeaf31
megha <megha.patel2@sheridancollege.ca>
2022-01-19 14:00:29 -0500

    added image file, .gitignore file modified personlist file

--- src/person/PersonList.java
+++ src/person/PersonList.java
@@ -15,7 +15,7 @@ public class PersonList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Person p=new Person();
     }
 
 }
